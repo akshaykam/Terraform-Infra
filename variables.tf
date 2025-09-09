83e458e4412819fff63c87479dc82110011474a7
akshaykam <akshaykam@cybage.com>
2025-09-09 19:35:18 +0530

    changed api-ui /template folder files

--- variables.tf
+++ variables.tf
@@ -19,6 +19,15 @@ variable "public_subnet_cidrs" {
   default = ["10.0.1.0/24", "10.0.2.0/24"] # two AZs
 }
 
-variable "node_group_min" { type = number default = 1 }
-variable "node_group_max" { type = number default = 2 }
-variable "node_instance_type" { type = string default = "t3.medium" }
+variable "node_group_min" { 
+  type = number 
+  default = 1 
+}
+variable "node_group_max" { 
+  type = number 
+  default = 2 
+}
+variable "node_instance_type" { 
+  type = string 
+  default = "t3.medium" 
+}
